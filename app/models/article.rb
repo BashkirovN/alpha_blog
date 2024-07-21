c7cdf223bbe34ec6c2287ac13914152ee189174d
Nikolai Bashkirov <bashkirov.office@gmail.com>
2024-07-21 00:21:06 -0700

    Add categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,10 @@
 class Article < ApplicationRecord
     # even an empty class is enough!
     belongs_to :user
+
+    has_many :article_categories
+    has_many :categories, through: :article_categories
+
     validates :title, presence: true, length: { minimum: 6, maximum: 100 }
     validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_19_203710) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_20_233915) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +23,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_19_203710) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
