1c8d17c7989c7661358a89c10b5bfd0929eb8567
Nikolai Bashkirov <bashkirov.office@gmail.com>
2024-07-22 17:27:29 -0700

    add admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
+
+      if user.id == 2
+        user.update(admin: true) 
+        flash[:notice] = "You are now Admin!"
+      end
       flash[:notice] = "Logged in successfully"
       redirect_to user
     else
